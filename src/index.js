750d4eac2fd6673f18bd7032bec2da1c26043585
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-07-07 00:25:22 +0530

    react starting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// render(what to show, where to show, callback)
+// jsx => js extension
+
+// {/* <> </> */} => react Fragment
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <>
+    <h1 className="hello"> Hello world! </h1>
+    <p> Hello para </p>
+    </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
